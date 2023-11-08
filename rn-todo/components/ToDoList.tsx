a52523b2c52277714621f0204a6f6c4cc068c0eb
justyasen <ymanahov@gmail.com>
2023-11-08 12:37:42 +0200

    Refactored code, made final adjustments to editing functionality.

--- rn-todo/components/ToDoList.tsx
+++ rn-todo/components/ToDoList.tsx
@@ -1,20 +1,21 @@
-import React, { useEffect, useState } from 'react';
-import { Button, FlatList, Keyboard, Modal, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import React, { useId, useState } from 'react';
+import { Button, Keyboard, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import DraggableFlatList from 'react-native-draggable-flatlist';
+import HapticFeedback from 'react-native-haptic-feedback';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { ToDoItem } from '../interfaces/ToDoItem';
-import HapticFeedback from 'react-native-haptic-feedback'; 
-import DraggableFlatList from 'react-native-draggable-flatlist';
 
 
 
 const ToDoList: React.FC = () => {
-  //This is a dummy array, would be replaced with dynamic in a real project.
+  let textInputRef: TextInput | null = null;
+
   const [todoItems, setTodoItems] = useState<ToDoItem[]>([]);
 
   const [newItemTitle, setNewItemTitle] = useState('');
   const [newItemDescription, setNewItemDescription] = useState('');
 
-  const [editingItemId, setEditingItemId] = useState<number | null>(null);
+  const [editingItemId, setEditingItemId] = useState<string | null>(null);
 
 
   const handleTitleChange = (text: string) => {
@@ -27,11 +28,11 @@ const ToDoList: React.FC = () => {
 
   const addItem = (title: string, description: string) => {
     if (title && description) {
+      const uniqueItemId = title.slice(0, 2) + description.slice(0, 2) + Math.random();
       const newItem = {
-        id: Math.random(),
+        id: uniqueItemId,
         title,
         description,
-        checked: false,
       };
   
       setTodoItems([...todoItems, newItem]);
@@ -45,7 +46,7 @@ const ToDoList: React.FC = () => {
     Keyboard.dismiss(); //improve the UI experience
   };
 
-  const editItem = (itemId: number) => {
+  const editItem = (itemId: string) => {
     setEditingItemId(itemId);
     const itemToEdit = todoItems.find((item) => item.id === itemId);
     if (itemToEdit) {
@@ -55,52 +56,50 @@ const ToDoList: React.FC = () => {
     }
   };
 
-  const saveItemChanges = (itemId: number, newTitle: string, newDescription: string) => {
+  const saveItemChanges = (itemId: string, newTitle: string, newDescription: string) => {
     // Create a copy of the todoItems array
     const updatedItems = [...todoItems];
   
     // Find the item with the matching ID
     const itemIndex = updatedItems.findIndex((item) => item.id === itemId);
-  
     // Update the item's title and description
     updatedItems[itemIndex].title = newTitle;
     updatedItems[itemIndex].description = newDescription;
-  
+    // Update the state
+    setTodoItems(updatedItems);
     // Clear the editing state
     setNewItemTitle('');
     setNewItemDescription('');
     setEditingItemId(null);
-  
-    // Update the state
-    setTodoItems(updatedItems);
-    
     Keyboard.dismiss();
   };
   
 
-  const deleteItem = (itemId: number) => {
+  const deleteItem = (itemId: string) => {
     const updatedItems = todoItems.filter((item) => item.id !== itemId);
     setTodoItems(updatedItems);
     HapticFeedback.trigger('notificationSuccess');
   };
 
-  const renderItem = ({item, drag}: {item: ToDoItem, drag: any}) => (
-    <TouchableOpacity style={styles.todoItem} onLongPress={drag}>
-      <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-        <Text style={styles.title}>{item.title}</Text>
-      </View>
-      <Text>{item.description}</Text>
-      {/* Edit button */}
-      <TouchableOpacity onPress={() => editItem(item.id)}> 
-        <Text style={styles.editButton}>Edit</Text>
+  const renderItem = ({item, drag}: {item: ToDoItem, drag: any}) => {
+    return (
+      <TouchableOpacity style={styles.todoItem} onLongPress={drag}>
+        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+          <Text style={styles.title}>{item.title}</Text>
+        </View>
+        <Text>{item.description}</Text>
+        {/* Edit button */}
+        <TouchableOpacity onPress={() => {editItem(item.id); textInputRef?.focus();}}> 
+          <Text style={styles.editButton}>Edit</Text>
+        </TouchableOpacity>
+        {/* Delete button */}
+        <TouchableOpacity onPress={() => deleteItem(item.id)}>
+          <Text style={styles.deleteButton}>Delete</Text>
+        </TouchableOpacity>
       </TouchableOpacity>
-      {/* Delete button */}
-      <TouchableOpacity onPress={() => deleteItem(item.id)}>
-        <Text style={styles.deleteButton}>Delete</Text>
-      </TouchableOpacity>
-    </TouchableOpacity>
-  );
-
+    )
+  };
+  
   return (
     <SafeAreaView>
       <Text style={styles.header}>
@@ -108,6 +107,7 @@ const ToDoList: React.FC = () => {
       </Text>
 
       <TextInput
+        ref={(ref) => (textInputRef = ref)}
         style={styles.textInputField}
         placeholder="Title"
         value={newItemTitle}
@@ -119,7 +119,10 @@ const ToDoList: React.FC = () => {
         value={newItemDescription}
         onChangeText={handleDescriptionChange}
       />
-      <Button title={editingItemId === null ? "Add Item" : "Save changes"} onPress={editingItemId === null ? () => addItem(newItemTitle, newItemDescription) : () => saveItemChanges(editingItemId, newItemTitle, newItemDescription)} />
+      <Button 
+        title={editingItemId === null ? "Add Item" : "Save changes"} 
+        onPress={editingItemId === null ? () => addItem(newItemTitle, newItemDescription) : () => saveItemChanges(editingItemId, newItemTitle, newItemDescription)} 
+      />
       
       {/* FlatList with all the ToDo items */}
       {todoItems.length === 0 ? (
@@ -130,7 +133,7 @@ const ToDoList: React.FC = () => {
       <DraggableFlatList
         keyboardShouldPersistTaps={'handled'} //to make sure items can be deleted even with keyboard open
         data={todoItems}
-        onDragEnd={({ data }) => setTodoItems(data)}
+        onDragEnd={({ data }) => {setTodoItems(data)}}
         keyExtractor={(item) => item.id.toString()}
         renderItem={renderItem}
       />
--- rn-todo/interfaces/ToDoItem.ts
+++ rn-todo/interfaces/ToDoItem.ts
@@ -1,5 +1,5 @@
 export interface ToDoItem {
-    id: number;
+    id: string;
     title: string;
     description: string;
 }
